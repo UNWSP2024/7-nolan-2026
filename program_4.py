88df76d5ea582e694f1ec8cc8dac1b987cc0a030
nolan-2026 <nrnelsen@students.unwsp.edu>
2026-03-05 14:30:12 -0600

    Update program_4.py

--- program_4.py
+++ program_4.py
@@ -8,3 +8,38 @@
 # Also include exception handling to deal with faulty input.
 # The distance between two points (x1,y1,z1) and (x2, y2, z2) is 
 #    given by:   sqrt ((x2-x1)^2 + (y2 - y1)^2 + (z1 - z2)^2) 
+
+import math
+
+def distance(coord1, coord2):
+    x1, y1, z1 = coord1
+    x2, y2, z2 = coord2
+
+    dist = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
+    return dist
+
+
+def main():
+    try:
+        print("Enter the first 3D coordinate:")
+        x1 = float(input("x1: "))
+        y1 = float(input("y1: "))
+        z1 = float(input("z1: "))
+        coord1 = (x1, y1, z1)
+
+        print("\nEnter the second 3D coordinate:")
+        x2 = float(input("x2: "))
+        y2 = float(input("y2: "))
+        z2 = float(input("z2: "))
+        coord2 = (x2, y2, z2)
+
+        # call distance function
+        point_distance = distance(coord1, coord2)
+
+        # display result
+        print("\nThe distance between the points is:", format(point_distance, '.2f'))
+
+    except ValueError:
+        print("Error: Invalid input")
+
+main()
